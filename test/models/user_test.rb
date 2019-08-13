816015e8da9d70713a56678a7dd1a400a8579311
Ngoc Truong <ngoc.truong@posteo.de>
2019-08-13 18:01:39 +0200

    Create first user test

---
+++ test/models/user_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  def setup 
+    @user = User.new( username: "Example User", 
+                      email: "user@example.com",
+                      password: "foobar")
+  end
+
+  test "should be valid" do 
+    assert @user.valid? 
+  end
+end
